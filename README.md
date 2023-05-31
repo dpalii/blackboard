796adff10114267a878ca5c2bf037b4985fb6b1c
Dmytro Palii <dpalii.study@gmail.com>
2023-05-31 17:17:21 +0300

    impl

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# blackboard
+# Blackboard
+
+In this example, we have three concrete knowledge components (KnowledgeComponentA, KnowledgeComponentB, KnowledgeComponentC) that inherit from the abstract KnowledgeComponent class. Each knowledge component implements the Update method, which takes a Blackboard instance as a parameter. The components retrieve data from the blackboard, perform their calculations or operations, and update the blackboard with the new results.
+
+The Blackboard class acts as the shared memory and holds the current state of the problem. It provides methods to retrieve and set data based on keys. The GetData method retrieves data of a specific type from the blackboard, and the SetData method sets data with a specific key.
+
+In the Main method, we create an instance of the blackboard and set initial data. Then, we create instances of the knowledge components and register them. Finally, we call the Update method on each component, passing the blackboard as an argument. Each component performs its specific task based on the data on the blackboard.
+
+The KnowledgeComponentC component displays the final result by retrieving the result from the blackboard and writing it to the console.
+
+This example demonstrates the basic implementation of the Blackboard pattern in C#, where knowledge components communicate and update a shared blackboard to solve a problem.
--- blackboard/Program.cs
+++ blackboard/Program.cs
@@ -1,2 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+// Component: Represents a piece of knowledge or expertise
+public abstract class KnowledgeComponent
+{
+    public abstract void Update(Blackboard blackboard);
+}
+
+// Blackboard: Acts as a shared memory that holds the current state of the problem and facilitates communication between components
+public class Blackboard
+{
+    private Dictionary<string, int> data;
+
+    public Blackboard()
+    {
+        data = new Dictionary<string, int>();
+    }
+
+    public int GetData(string key)
+    {
+        if (data.ContainsKey(key))
+        {
+            return data[key];
+        }
+
+        return 0;
+    }
+
+    public void SetData(string key, int value)
+    {
+        data[key] = value;
+    }
+}
+
+// Concrete Knowledge Components
+public class KnowledgeComponentA : KnowledgeComponent
+{
+    public override void Update(Blackboard blackboard)
+    {
+        // Retrieve data from the blackboard
+        int dataA = blackboard.GetData("DataA");
+
+        // Perform some calculations or operations
+        int result = dataA * 2;
+
+        // Update the blackboard with the new result
+        blackboard.SetData("Result", result);
+    }
+}
+
+public class KnowledgeComponentB : KnowledgeComponent
+{
+    public override void Update(Blackboard blackboard)
+    {
+        // Retrieve data from the blackboard
+        int dataB = blackboard.GetData("DataB");
+
+        // Perform some calculations or operations
+        int result = dataB + 5;
+
+        // Update the blackboard with the new result
+        blackboard.SetData("Result", result);
+    }
+}
+
+public class KnowledgeComponentC : KnowledgeComponent
+{
+    public override void Update(Blackboard blackboard)
+    {
+        // Retrieve data from the blackboard
+        int result = blackboard.GetData("Result");
+
+        // Display the final result
+        Console.WriteLine($"Final Result: {result}");
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        // Create the blackboard
+        var blackboard = new Blackboard();
+
+        // Set initial data on the blackboard
+        blackboard.SetData("DataA", 10);
+        blackboard.SetData("DataB", 7);
+
+        // Create and register the knowledge components
+        var componentA = new KnowledgeComponentA();
+        var componentB = new KnowledgeComponentB();
+        var componentC = new KnowledgeComponentC();
+
+        // Update the knowledge components
+        componentA.Update(blackboard);
+        componentB.Update(blackboard);
+        componentC.Update(blackboard);
+
+        Console.ReadLine();
+    }
+}
